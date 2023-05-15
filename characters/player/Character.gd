5142de364dd6f4b6515d63b8653ea6bc699eef9d
joshuagray0212 <joshuagray0212@gmail.com>
2023-05-15 14:59:23 -0400

    consolidated character to its own node

--- characters/player/Character.gd
+++ characters/player/Character.gd
@@ -1,28 +1,34 @@
 extends CharacterBody2D
 
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+@export var speed = 400.0
+const JUMP_VELOCITY = -700.0
+signal text
+var apple = false
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
-var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+func get_input():
+	var input_direction = Input.get_vector("left", "right", "up", "down")
+	velocity = input_direction * speed
 
+#func attack():
+	#if Input.is_action_just_released("attack"):
+		#get_node("AttackTemp").show()
+		#get_node("AttackTemp/TempTimer").start()
 
 func _physics_process(delta):
-	# Add the gravity.
-	if not is_on_floor():
-		velocity.y += gravity * delta
-
-	# Handle Jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-
+	#attack()
+	get_input()
 	move_and_slide()
+
+#func _on_area_2d_body_entered(body):
+	#if apple == true:
+		#text.emit()
+
+
+#func _on_timer_timeout():
+	#apple = true
+
+
+#func _on_temp_timer_timeout():
+	#get_node("AttackTemp").hide()
